d9f2a46604d5a7f369aefefab34ba68679a37723
Isagani Lapira <lapiraisagani@gmail.com>
2024-08-30 14:33:31 +0800

    Fixed: Registration no validation response

--- src/main/java/com/eleganteeshop/Elegantee/Shop/Dto/AccountDetailDTO.java
+++ src/main/java/com/eleganteeshop/Elegantee/Shop/Dto/AccountDetailDTO.java
@@ -1,13 +1,22 @@
 package com.eleganteeshop.Elegantee.Shop.Dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
 public class AccountDetailDTO {
+
+    @NotEmpty
     private String firstname;
+    @NotEmpty
     private String lastname;
+    @Email
     private String emailAddress;
+    @Past
     private LocalDate birthDate;
 }
--- src/main/java/com/eleganteeshop/Elegantee/Shop/Dto/RegistrationBaseDTO.java
+++ src/main/java/com/eleganteeshop/Elegantee/Shop/Dto/RegistrationBaseDTO.java
@@ -1,12 +1,15 @@
 package com.eleganteeshop.Elegantee.Shop.Dto;
 
 import com.eleganteeshop.Elegantee.Shop.entities.UserEntity;
+import jakarta.validation.Valid;
 import lombok.Data;
 
 
 @Data
 public class RegistrationBaseDTO {
 
+    @Valid
     private UserEntity user;
+    @Valid
     private AccountDetailDTO accountDetails;
 }
